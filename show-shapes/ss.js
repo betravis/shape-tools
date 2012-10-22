86d24130cedbadc213ce9ee29fd98c098e19c491
Bear Travis <betravis@adobe.com>
2012-10-22 16:38:27 -0700

    Removing button, minifying min.js

--- show-shapes/ss.js
+++ show-shapes/ss.js
@@ -1,7 +1,6 @@
 /**
  * Show Shapes Bookmarklet
  * Copyright (C) 2012
- * This file will not be used until I start actually mini-fying ss.min.js
  */
 (function() {
 function format() {
@@ -11,10 +10,10 @@ function format() {
         return (num >= 0 && num < outerArgs.length - 1) ? outerArgs[num + 1] : '';
     });
 }
-function shapeInsideToSVG(shapeInside) {
+function shapeInsideToSVG(shapeInside, elem, style) {
     shapeInside = shapeInside.split(/[()]/);
     var command = shapeInside[0];
-    var args = shapeInside[1].split(/,\s*/);
+    var args = shapeInside[1].split(/\s*,\s*/);
     var result = '';
     switch(command) {
         case 'rectangle':
@@ -33,7 +32,21 @@ function shapeInsideToSVG(shapeInside) {
             break;
         case 'polygon':
             var fillRule = (args[0].search(/nonzero|evenodd/i) >= 0 ? args.shift() : "nonzero");
-            var points = args.reduce(function(prev, curr, index, arr) { return prev + (index > 0 ? " " : "") + curr.replace(/\s+/, ",").replace(/px/g, ""); }, "");
+            var points = args.map(function(curr, index, arr) {
+                curr = curr.replace(/\s+/, ',');
+                curr = curr.replace(/(-?\d+(?:\.\d+)?)(px|%)/g, function($0, $1, $2, offset) {
+                    var digit = parseFloat($1);
+                    switch ($2) {
+                        case '%':
+                            digit *= (offset ? elem.offsetHeight : elem.offsetWidth) / 100;
+                        case 'px':
+                        default:
+                    }
+                    return digit.toString();
+                });
+                return curr;
+            });
+            points = points.join(' ');
             result = format("<polygon fill-rule='{0}' points='{1}' />", fillRule, points);
             break;
         default:
@@ -42,51 +55,40 @@ function shapeInsideToSVG(shapeInside) {
     return "<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><g fill='blue'>" + result + "</g></svg>";
 }
 function showShapeInside(elem, style, shape) {
-    var svg = shapeInsideToSVG(shape);
+    var svg = shapeInsideToSVG(shape, elem, style);
     if (style.getPropertyValue('position') == 'static')
         elem.style.setProperty('position', 'relative');
     var div = document.createElement('div');
     div.setAttribute('class', 'shape-inside-bookmarklet-overlay');
-    div.setAttribute('style', 'width:100%;height:100%;position:absolute;top:0;opacity:0.5');
+    div.setAttribute('style', 'width:100%;height:100%;position:absolute;top:0;left:0;opacity:0.5');
     div.innerHTML = svg;
     elem.appendChild(div);
 }
 function showShapeInsides() {
     var elems = document.getElementsByTagName('*');
-    for (var i in elems) {
-        var style = getComputedStyle(elems[i]);
-        if (style && style.getPropertyValue('-webkit-shape-inside') != 'auto')
-            showShapeInside(elems[i], style, style.getPropertyValue('-webkit-shape-inside'));
-    }    
-}
-var button = null;
-function startBookmarklet() {
-    if (button)
-        return;
-    button = document.createElement('button');
-    button.setAttribute('class', 'btn btn-inverse');
-    button.setAttribute('style', 'position:fixed;top:0;right:0');
-    button.innerHTML = 'Exit ShowShapes';
-    button.onclick = endBookmarklet;
-    document.body.appendChild(button);
-    showShapeInsides();
-}
-function endBookmarklet() {
-    document.body.removeChild(button);
-    button = null;
-    var elems = document.getElementsByClassName('shape-inside-bookmarklet-overlay');
     for (var i = 0; i < elems.length; i++) {
         var elem = elems[i];
-        elem.parentNode.removeChild(elem);
+        var style = getComputedStyle(elem);
+        if (style && style.getPropertyValue('-webkit-shape-inside') != 'auto')
+            showShapeInside(elem, style, style.getPropertyValue('-webkit-shape-inside'));
     }
 }
+var shapesOn = false;
+function startBookmarklet() {
+    showShapeInsides();
+    shapesOn = true;
+}
+function endBookmarklet() {
+    var elems = document.getElementsByClassName('shape-inside-bookmarklet-overlay');
+    /* elems updates with modifications */
+    while (elems.length) {
+        var elem = elems[0];
+        elem.parentNode.removeChild(elem);
+    }
+    shapesOn = false;
+}
 !function() {
-    var link = document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('type', 'text/css');
-    link.setAttribute('href', 'http://betravis.github.com/shape-tools/css/bootstrap.min.css');
-    document.getElementsByTagName('head')[0].appendChild(link);
-    window.showShapesBookmarklet = startBookmarklet;
+    window.showShapesBookmarklet = function() { shapesOn ? endBookmarklet() : startBookmarklet(); }
     startBookmarklet();
 }()
 })()
